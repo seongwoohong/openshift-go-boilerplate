71a4aa33cfa5940bd6ca944c04a8215db3c90b45
Seongwoo Hong <seongwoo.hong@duke.edu>
2020-12-17 18:36:05 -0500

    Initial Commit
    This is just a placeholder repo

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	response := os.Getenv("RESPONSE")
+	if len(response) == 0 {
+		response = "Hello Duke OKD!"
+	}
+
+	fmt.Fprintln(w, response)
+  ip := GetIP(r)
+	fmt.Println("Servicing request at " + time.Now().String() + " towards " + ip + " " + r.RemoteAddr)
+}
+
+func listenAndServe(port string) {
+	fmt.Printf("serving on %s\n", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
+
+func GetIP(r *http.Request) string {
+	forwarded := r.Header.Get("X-FORWARDED-FOR")
+	if forwarded != "" {
+		return forwarded
+	}
+	return r.RemoteAddr
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	go listenAndServe(port)
+
+	select {}
+}
